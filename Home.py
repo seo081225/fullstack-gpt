016fc3d7903c3756b6bd7ffb16839d794b7f1916
seo <seo081225@gmail.com>
2024-08-29 21:46:49 +0900

    #7.4 Chat Messages

--- Home.py
+++ Home.py
@@ -5,19 +5,19 @@ st.set_page_config(
     page_icon="🤖",
 )
 
-
-with st.sidebar:
-    st.title("sidebar title")
-    st.text_input("xxx")
-
-tab_one, tab_two, tab_three = st.tabs(["A", "B", "C"])
-
-with tab_one:
-    st.write("a")
-
-with tab_two:
-    st.write("b")
-
-with tab_three:
-    st.write("c")
-    
+st.markdown(
+    """
+# Hello!
+            
+Welcome to my FullstackGPT Portfolio!
+            
+Here are the apps I made:
+            
+- [ ] [DocumentGPT](/DocumentGPT)
+- [ ] [PrivateGPT](/PrivateGPT)
+- [ ] [QuizGPT](/QuizGPT)
+- [ ] [SiteGPT](/SiteGPT)
+- [ ] [MeetingGPT](/MeetingGPT)
+- [ ] [InvestorGPT](/InvestorGPT)
+"""
+)
